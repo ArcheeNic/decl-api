cf1b4a7dc45a50af7f6d3abf698d570a4b2d0f35
Arthur Kasimov <kasimovarthur@gmail.com>
2019-02-27 22:46:58 +0300

    Исправление соместимости с 7.0

--- src/Documentor/ItemPoint.php
+++ src/Documentor/ItemPoint.php
@@ -24,15 +24,35 @@ class ItemPoint extends ItemInfo
     public $docFile;
 
     /**
-     * @throws \ReflectionException
+     * @throws \Exception
      */
     function analyse()
     {
-        $classname      = $this->classname;
-        $reflection     = new \ReflectionClass($classname);
-        $handlerMethod  = $reflection->getMethod('handler');
-        $this->response = $handlerMethod->getReturnType()->getName();
-        $this->request  = $handlerMethod->getParameters()[0]->getType()->getName();
+        $classname     = $this->classname;
+        $reflection    = new \ReflectionClass($classname);
+        $handlerMethod = $reflection->getMethod('handler');
+
+        $this->response = (string)$handlerMethod->getReturnType();
+
+        if (!$this->response) {
+            throw new \Exception('Отсутсвует информация о response типе объекта класса .'.$classname);
+        }
+
+        $requestParameters = $handlerMethod->getParameters();
+        if (!$requestParameters) {
+            throw new \Exception('Отсутствует информация о request объекте класса .'.$classname);
+        }
+
+        $requestType = $requestParameters[0];
+        if (!$requestType) {
+            throw new \Exception('Отсутствует информация о request типе объекта класса .'.$classname);
+        }
+
+        $this->request = (string) $requestType->getType();
+        if (!$this->request) {
+            throw new \Exception('Отсутствует информация о request типе объекта класса .'.$classname);
+        }
+
         $this->docFile = $classname::$docFile;
     }
 }
