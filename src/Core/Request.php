fc9cca54106e0b88d7e8872646fe3588e077ee84
Arthur Kasimov <kasimovarthur@gmail.com>
2019-03-09 23:52:39 +0300

    Исключён лишний метод, удаленный ранее.

--- src/Core/Request.php
+++ src/Core/Request.php
@@ -117,7 +117,6 @@ abstract class Request extends ObjectClass
         $target = null
     ): \Illuminate\Contracts\Validation\Validator {
         if ($target) {
-            $this->validateStrictRunTarget($target);
             return (new ValidatorFactory())->make($this->dataRaw[$target] ?? [],
                 $this->rulesInfo()->rulesGroup($target));
         }
