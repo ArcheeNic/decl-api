18c0e97a7fdf371e8fcdf4fd16b969c0b4e83357
Arthur Kasimov <kasimovarthur@gmail.com>
2020-07-27 07:50:39 +0300

    fix ошибки. parameterRoute. не был прописан как обрабатываемое свойство

--- src/Core/RulesInfoRequest.php
+++ src/Core/RulesInfoRequest.php
@@ -14,6 +14,7 @@ class RulesInfoRequest
     protected $data
         = [
             'parameter' => [],
+            'parameterRoute' => [],
             'header'    => [],
             'cookie'    => [],
             'json'      => []
